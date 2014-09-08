bd0f9f0159a165651ad38dfd40b2b136974aabb8
Jason K. Moore <moorepants@gmail.com>
2014-09-08 17:29:10 -0400

    Preserve hardlinks on offsite copy.

--- backup_sites.sh
+++ backup_sites.sh
@@ -2,5 +2,5 @@
 # from hmc.csuohio.edu to an offsite location.
 for DIR in backups blobstoragebackups blobstoragesnapshots snapshotbackups
 do
-    rsync -a moorepants@hmc.csuohio.edu:/home/moorepants/tmp_backup/$DIR /home/jasonkmoore/website-backups/hmc.csuohio.edu/
+    rsync -aH moorepants@hmc.csuohio.edu:/home/moorepants/tmp_backup/$DIR /home/jasonkmoore/website-backups/hmc.csuohio.edu/
 done
