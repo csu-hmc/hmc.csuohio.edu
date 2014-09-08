e21e34ab1b18e479f2f2c524605cb0980473a396
Jason K. Moore <moorepants@gmail.com>
2014-09-08 09:46:37 -0400

    Trying cron vrom recipe at every hour.

--- copy_backup_to_home.sh
+++ copy_backup_to_home.sh
@@ -1,8 +1,14 @@
 for DIR in backups blobstoragebackups blobstoragesnapshots snapshotbackups
 do
 	rm -rf /home/moorepants/tmp_backup/$DIR
+	#cp -r --no-preserve=ownership /usr/local/Plone/zinstance/var/$DIR /home/moorepants/tmp_backup
 	cp -r /usr/local/Plone/zinstance/var/$DIR /home/moorepants/tmp_backup
 done
-# This is to remove the plone_daemon:plone_group permissions so that moorepants
-# has permissions to download them from a remote server.
-chown -R moorepants:moorepants /home/moorepants/tmp_backup
+# The user plone_buildout runs this script so it does not have permissions
+# enough to change all the files because they are owned by
+# plone_daemon:plone_group so the following command fails.
+# If the --no-preseve=ownership flag is used in cp then the owner is
+# plone_buildout:plone_group, so it should now be able to modify the files
+# ownership but that doesn't seem to happen.
+#chmod -r g+rw /home/moorepants/tmp_backup
+#chown -R moorepants:hmc_backup /home/moorepants/tmp_backup
